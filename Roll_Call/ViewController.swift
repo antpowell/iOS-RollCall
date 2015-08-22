552b0f3788076d2d2e181449c1d208641f4a554e
Anthony Powell <anthonypowell@tsutechs-MacBook-Pro.local>
2015-08-22 12:59:06 -0500

    Initial Commit

---
+++ Roll_Call/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roll_Call
+//
+//  Created by Anthony Powell on 8/22/15.
+//  Copyright (c) 2015 Anthony Powell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
