fbd9535b144e2d8dfbe5b0c18b1ac95fe9d008f2
antpowell <powell.anthony.e@gmail.com>
2017-11-22 16:02:38 -0600

    Link create account pages with login page

---
+++ HollowRoundedBlackButton.swift
@@ -0,0 +1,22 @@
+//
+//  HollowRoundedBlackButton.swift
+//  Roll_Call
+//
+//  Created by Anthony Powell on 11/22/17.
+//  Copyright © 2017 Anthony Powell. All rights reserved.
+//
+
+import UIKit
+
+class HollowRoundedBlackButton: UIButton {
+
+    private var radius = 7
+    
+    required init?(coder aDecoder: NSCoder) {
+        super .init(coder: aDecoder)
+        layer.cornerRadius = CGFloat(radius)
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+    }
+
+}
